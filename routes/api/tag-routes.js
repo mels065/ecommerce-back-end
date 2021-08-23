54facb0954b26beb9cb4670a6761fab2096f8a18
Brandon Mellus <mellusbrandon@gmail.com>
2021-08-22 21:57:28 -0400

    Create GET route for getting all products

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,35 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Category, Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+const opts = {
+  include: [
+    {
+      model: Product,
+      attributes: [
+        'id',
+        'product_name',
+        'price',
+        'stock'
+      ],
+      through: { attributes: [] },
+      include: [
+        {
+          model: Category
+        }
+      ]
+    }
+  ]
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll(opts);
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
